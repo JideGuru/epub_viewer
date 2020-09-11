350674a49178520b80b06dc580391c4a6272764b
Restyled.io <commits@restyled.io>
2020-09-11 18:26:59 +0000

    Restyled by clang-format

--- android/src/main/java/com/jideguru/epub_viewer/ReaderConfig.java
+++ android/src/main/java/com/jideguru/epub_viewer/ReaderConfig.java
@@ -3,35 +3,36 @@ package com.jideguru.epub_viewer;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.Log;
-
 import com.folioreader.Config;
 import com.folioreader.util.AppUtil;
 
 public class ReaderConfig {
-    private String identifier;
-    private String themeColor;
-    private String scrollDirection;
-    private boolean allowSharing;
-    private boolean showTts;
+  private String identifier;
+  private String themeColor;
+  private String scrollDirection;
+  private boolean allowSharing;
+  private boolean showTts;
 
-    public Config config;
+  public Config config;
 
-    public ReaderConfig(Context context, String identifier, String themeColor,
-                        String scrollDirection, boolean allowSharing, boolean showTts){
+  public ReaderConfig(Context context, String identifier, String themeColor,
+                      String scrollDirection, boolean allowSharing,
+                      boolean showTts) {
 
-//        config = AppUtil.getSavedConfig(context);
-//        if (config == null)
-            config = new Config();
-        if (scrollDirection.equals("vertical")){
-            config.setAllowedDirection(Config.AllowedDirection.ONLY_VERTICAL);
-        }else if(scrollDirection.equals("horizontal")){
-            config.setAllowedDirection(Config.AllowedDirection.ONLY_HORIZONTAL);
-        }else{
-            config.setAllowedDirection(Config.AllowedDirection.VERTICAL_AND_HORIZONTAL);
-        }
-        config.setThemeColorInt(Color.parseColor(themeColor));
-        config.setNightThemeColorInt(Color.parseColor(themeColor));
-        config.setShowRemainingIndicator(true);
-        config.setShowTts(showTts);
+    //        config = AppUtil.getSavedConfig(context);
+    //        if (config == null)
+    config = new Config();
+    if (scrollDirection.equals("vertical")) {
+      config.setAllowedDirection(Config.AllowedDirection.ONLY_VERTICAL);
+    } else if (scrollDirection.equals("horizontal")) {
+      config.setAllowedDirection(Config.AllowedDirection.ONLY_HORIZONTAL);
+    } else {
+      config.setAllowedDirection(
+          Config.AllowedDirection.VERTICAL_AND_HORIZONTAL);
     }
+    config.setThemeColorInt(Color.parseColor(themeColor));
+    config.setNightThemeColorInt(Color.parseColor(themeColor));
+    config.setShowRemainingIndicator(true);
+    config.setShowTts(showTts);
+  }
 }
