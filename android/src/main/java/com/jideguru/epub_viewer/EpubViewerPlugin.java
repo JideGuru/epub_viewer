ad9b9ccfd73d09e45ba4a574ea0fa10c5e839947
jideguru <guruliciousjide@gmail.com>
2020-11-11 16:27:11 +0100

    Added readium dependencies to android

--- android/src/main/java/com/jideguru/epub_viewer/EpubViewerPlugin.java
+++ android/src/main/java/com/jideguru/epub_viewer/EpubViewerPlugin.java
@@ -15,8 +15,6 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** EpubReaderPlugin */
 public class EpubViewerPlugin implements MethodCallHandler {
 
-  private Reader reader;
-  private ReaderConfig config;
 
   static private Activity activity;
   static private Context context;
@@ -37,27 +35,27 @@ public class EpubViewerPlugin implements MethodCallHandler {
   public void onMethodCall(MethodCall call, Result result) {
 
     if (call.method.equals("setConfig")){
-      Map<String,Object> arguments = (Map<String, Object>) call.arguments;
-      String identifier = arguments.get("identifier").toString();
-      String themeColor = arguments.get("themeColor").toString();
-      String scrollDirection = arguments.get("scrollDirection").toString();
-      Boolean nightMode = Boolean.parseBoolean(arguments.get("nightMode").toString());
-      Boolean allowSharing = Boolean.parseBoolean(arguments.get("allowSharing").toString());
-      Boolean enableTts = Boolean.parseBoolean(arguments.get("enableTts").toString());
-      config = new ReaderConfig(context,identifier,themeColor,
-              scrollDirection,allowSharing, enableTts,nightMode);
+//      Map<String,Object> arguments = (Map<String, Object>) call.arguments;
+//      String identifier = arguments.get("identifier").toString();
+//      String themeColor = arguments.get("themeColor").toString();
+//      String scrollDirection = arguments.get("scrollDirection").toString();
+//      Boolean nightMode = Boolean.parseBoolean(arguments.get("nightMode").toString());
+//      Boolean allowSharing = Boolean.parseBoolean(arguments.get("allowSharing").toString());
+//      Boolean enableTts = Boolean.parseBoolean(arguments.get("enableTts").toString());
+//      config = new ReaderConfig(context,identifier,themeColor,
+//              scrollDirection,allowSharing, enableTts,nightMode);
 
     } else if (call.method.equals("open")){
 
       Map<String,Object> arguments = (Map<String, Object>) call.arguments;
       String bookPath = arguments.get("bookPath").toString();
       String lastLocation = arguments.get("lastLocation").toString();
-
-      reader = new Reader(context,messenger,config);
-      reader.open(bookPath, lastLocation);
+//
+//      reader = new Reader(context,messenger,config);
+//      reader.open(bookPath, lastLocation);
 
     }else if(call.method.equals("close")){
-      reader.close();
+//      reader.close();
     }
 
     else {
